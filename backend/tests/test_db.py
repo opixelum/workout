837e2f2873859c75f7d0dbdf61be9ab7830876bd
opixelum <48217573+opixelum@users.noreply.github.com>
2026-08-16 20:01:18 +0200

    tests(backend): add macrocycle lifecycle test

--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -76,3 +76,99 @@ def test_user_lifecycle(db_session):
     assert user is None
 
     db_session.close()
+
+
+def test_macrocycle_lifecycle(db_session):
+    # Create user (required for macrocycle)
+    user_in = schemas.UserCreate(
+        email="macrocycle_test@example.com", password="password", bodyweight=80.0
+    )
+    user = crud.create_user(db_session, user_in)
+    user_id = user.id
+
+    name = "Test Macrocycle"
+    description = "A test macrocycle"
+    nb_mesocycles = 4
+    new_name = "Updated Macrocycle"
+    new_description = "An updated test macrocycle"
+    new_nb_mesocycles = 6
+
+    # Create macrocycle
+    macrocycle_in = schemas.MacrocycleCreate(
+        name=name,
+        user_id=user_id,
+        description=description,
+        nb_mesocycles=nb_mesocycles,
+    )
+    crud.create_macrocycle(db_session, macrocycle_in)
+
+    # Get macrocycles by user id
+    macrocycles = crud.get_macrocycles(db_session, user_id=user_id)
+    assert len(macrocycles) == 1
+    macrocycle = macrocycles[0]
+    assert macrocycle.id is not None
+    assert macrocycle.user_id == user_id
+    assert macrocycle.name == name
+    assert macrocycle.description == description
+    assert macrocycle.nb_mesocycles == nb_mesocycles
+    assert macrocycle.creation_date is not None
+
+    # Get macrocycle by id
+    macrocycle_id = macrocycle.id
+    macrocycle = crud.get_macrocycle(db_session, macrocycle_id)
+    assert macrocycle is not None
+    assert macrocycle.id == macrocycle_id
+    assert macrocycle.user_id == user_id
+    assert macrocycle.name == name
+    assert macrocycle.description == description
+    assert macrocycle.nb_mesocycles == nb_mesocycles
+
+    # Update name
+    macrocycle_in = schemas.MacrocycleUpdate(name=new_name)
+    crud.update_macrocycle(db_session, macrocycle_id, macrocycle_in)
+    updated_macrocycle = crud.get_macrocycle(db_session, macrocycle_id)
+    assert updated_macrocycle is not None
+    assert updated_macrocycle.id == macrocycle_id
+    assert updated_macrocycle.name == new_name
+    assert updated_macrocycle.description == description
+    assert updated_macrocycle.nb_mesocycles == nb_mesocycles
+
+    # Update description
+    macrocycle_in = schemas.MacrocycleUpdate(description=new_description)
+    crud.update_macrocycle(db_session, macrocycle_id, macrocycle_in)
+    updated_macrocycle = crud.get_macrocycle(db_session, macrocycle_id)
+    assert updated_macrocycle is not None
+    assert updated_macrocycle.id == macrocycle_id
+    assert updated_macrocycle.name == new_name
+    assert updated_macrocycle.description == new_description
+    assert updated_macrocycle.nb_mesocycles == nb_mesocycles
+
+    # Update nb_mesocycles
+    macrocycle_in = schemas.MacrocycleUpdate(nb_mesocycles=new_nb_mesocycles)
+    crud.update_macrocycle(db_session, macrocycle_id, macrocycle_in)
+    updated_macrocycle = crud.get_macrocycle(db_session, macrocycle_id)
+    assert updated_macrocycle is not None
+    assert updated_macrocycle.id == macrocycle_id
+    assert updated_macrocycle.name == new_name
+    assert updated_macrocycle.description == new_description
+    assert updated_macrocycle.nb_mesocycles == new_nb_mesocycles
+
+    # Update everything
+    macrocycle_in = schemas.MacrocycleUpdate(
+        name=name, description=description, nb_mesocycles=nb_mesocycles
+    )
+    crud.update_macrocycle(db_session, macrocycle_id, macrocycle_in)
+    updated_macrocycle = crud.get_macrocycle(db_session, macrocycle_id)
+    assert updated_macrocycle is not None
+    assert updated_macrocycle.id == macrocycle_id
+    assert updated_macrocycle.name == name
+    assert updated_macrocycle.description == description
+    assert updated_macrocycle.nb_mesocycles == nb_mesocycles
+
+    # Delete macrocycle
+    crud.delete_macrocycle(db_session, macrocycle_id)
+    macrocycle = crud.get_macrocycle(db_session, macrocycle_id)
+    assert macrocycle is None
+
+    crud.delete_user(db_session, user_id)
+    db_session.close()
